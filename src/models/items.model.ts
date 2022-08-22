af75ccb9e88cb1928e27de2872d4fef454799f2a
Julian Villegas <jville7@bancodebogota.com.co>
2022-08-21 22:30:48 -0500

    [UPD]product.service: Adding category_id on get item data

--- src/models/items.model.ts
+++ src/models/items.model.ts
@@ -1,33 +1,34 @@
 export interface ProductsRsModel {
-    author: AuthorModel;
-    categories: string[];
-    items: ItemModel[];
+	author: AuthorModel;
+	categories: string[];
+	items: ItemModel[];
 }
 
 export interface ProductRsModel {
-    author: AuthorModel;
-    item: ItemModel;
+	author: AuthorModel;
+	item: ItemModel;
 }
 
 export interface AuthorModel {
-    id?: string;
-    name: string;
-    lastName: string;
+	id?: string;
+	name: string;
+	lastName: string;
 }
 
 export interface ItemModel {
-    id: string;
-    title: string;
-    price: CurrencyModel;
-    picture: string;
-    condition: string;
-    freeShipping: boolean;
-    soldQuantity?: number;
-    description?: string;
+	id: string;
+	title: string;
+	price: CurrencyModel;
+	picture: string;
+	condition: string;
+	freeShipping: boolean;
+	soldQuantity?: number;
+	description?: string;
+	categoryId?: string;
 }
 
 export interface CurrencyModel {
-    currency: string;
-    amount: number;
-    decimals: number;
+	currency: string;
+	amount: number;
+	decimals: number;
 }
